706a93a9cf06840d43606a28415cdc8cd7e6b6d8
mohammad <aref.nouri.a@gmail.com>
2022-05-22 20:11:11 +1000

    lool

---
+++ index.js
@@ -0,0 +1,26 @@
+// class Animator {
+//     constructor(selector) {
+//         this.selector = document.querySelector(selector);
+//     }
+//     fadeOut() {
+//         this.selector.style.transition = "all 450ms ease";
+//         this.selector.style.opacity = 1;
+//     }
+// }
+
+// const logo = new Animator(".logo-text");
+
+// logo.fadeOut();
+
+
+// const validatingLogIn = () => {
+//     let username = document.getElementsByClassName(".user-email").value;
+//     let password = document.getElementsByClassName(".user-password").value;
+
+//     if(username === "admin@adminpanel.com" && password === "strongpassword"){
+//         let btn = document.getElementsByClassName(".submit-btn");
+
+//     }
+
+
+// }
